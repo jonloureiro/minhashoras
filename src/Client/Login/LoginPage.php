d13ca31405eaf6f4070863d0d8757763954b42b0
jonathan.loureiro.m@gmail.com <me@jonloureiro.dev>
2020-03-04 20:13:33 -0400

    limpando "use"s desnecessários

--- src/Client/Login/LoginPage.php
+++ src/Client/Login/LoginPage.php
@@ -4,11 +4,6 @@ declare(strict_types=1);
 
 namespace App\Client\Login;
 
-use League\Plates\Engine;
-use Psr\Http\Message\ResponseFactoryInterface;
-use Psr\Http\Message\ResponseInterface;
-use Psr\Http\Message\ServerRequestInterface;
-
 class LoginPage
 {
     public function __invoke(): array
