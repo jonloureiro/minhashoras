43abdb4b5e3cb35143c5356d63eac286dad83427
jonathan.loureiro.m@gmail.com <me@jonloureiro.dev>
2020-03-06 14:08:50 -0400

    adicionando log durante o processo de minify

--- assets/index.php
+++ assets/index.php
@@ -18,13 +18,17 @@ $cssfiles = array_diff(scandir($csspath), $ignore);
 foreach ($jsfiles as $file) {
     if ($pos = strrpos($file, '.js')) {
         $name = substr($file, 0, $pos);
-        (new Minify\JS("$jspath/$file"))->minify("$path/dist/$name.min.js");
+        $newFile = "$path/dist/$name.min.js";
+        echo "Minify js: $newFile\n";
+        (new Minify\JS("$jspath/$file"))->minify($newFile);
     }
 }
 
 foreach ($cssfiles as $file) {
     if ($pos = strrpos($file, '.css')) {
         $name = substr($file, 0, $pos);
-        (new Minify\CSS("$csspath/$file"))->minify("$path/dist/$name.min.css");
+        $newFile = "$path/dist/$name.min.css";
+        echo "Minify css: $newFile\n";
+        (new Minify\CSS("$csspath/$file"))->minify($newFile);
     }
 }
